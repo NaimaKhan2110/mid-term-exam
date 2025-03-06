c51e435af087b716f24bea64dd14a79c4031a53b
NaimaKhan2117 <naimakhan2117@gmail.com>
2025-03-06 23:22:32 +0600

    Update requirements.txt with latest dependencies

--- manage.py
+++ manage.py
@@ -1,22 +1,11 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+    
+    # Get the port from the environment variable (default 8000)
+    port = os.environ.get("PORT", "8000")
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_management.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+    from django.core.management import execute_from_command_line
+    execute_from_command_line([sys.argv[0], "runserver", f"0.0.0.0:{port}"])
